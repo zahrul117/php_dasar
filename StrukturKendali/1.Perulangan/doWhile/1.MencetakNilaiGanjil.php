aa919f877d09cf925655206bdce07db36f0b2d07
zahrul117 <zahrul0009@gmail.com>
2025-05-08 15:23:29 +0700

    commit Kedua Saya Mengulangi materi perulangan

---
+++ StrukturKendali/1.Perulangan/doWhile/1.MencetakNilaiGanjil.php
@@ -0,0 +1,12 @@
+<?php
+$i = 1;
+
+do{
+    if($i % 2 == 1){
+        echo "$i Adalah Ganjil " . "<br>";
+    }
+    $i++;
+}while($i <= 15);
+
+
+?>
---
+++ StrukturKendali/1.Perulangan/for/3.genapDari10Hingga100.php
@@ -0,0 +1,8 @@
+<?php
+for($i = 10; $i <= 100; $i++){
+    if($i % 2 == 0){
+        echo "nilai $i adalah genap" . "<br>";
+    }
+}
+
+?>
