af3a58d948198a5397888675c489ad5d4b8f8dcc
zahrul117 <zahrul0009@gmail.com>
2025-05-19 19:55:07 +0700

    Belajar Searching

---
+++ Searching/index.php
@@ -0,0 +1,59 @@
+<?php
+require 'functions.php';
+
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+// tombol cari ditekan
+if(isset($_POST['cari'])){
+    $mahasiswa = cari($_POST['keyword']);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Daftar Mahasiswa </h1>
+    <a href="tambah.php">Tambah data mahasiswa</a>
+    <br><br>
+    <form action="" method="post">
+        <input type="text" name="keyword" autofocus placeholder="Cari mahasiswa" autocomplete="off">
+        <button type="submit" name="cari">Cari!</button>
+    </form>
+    <br>
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>NRP</th>
+            <th>nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+        </tr>
+        <?php $i = 1 ?>
+        <?php foreach ($mahasiswa as $mhs): ?>
+            <tr>
+                <td><?= $i; ?></td>
+                <td>
+                    <a href="ubah.php?id=<?= $mhs['id']?>">Ubah</a> |
+                    <a href="hapus.php?id=<?= $mhs['id'] ?>" onclick="return confirm('Yakin?')">Hapus</a>
+                </td>
+                <td><img src="img/<?= $mhs['gambar']; ?>" alt="" width="75"></td>
+                <td><?= $mhs['nrp'] ?></td>
+                <td><?= $mhs['nama'] ?></td>
+                <td><?= $mhs['email'] ?></td>
+                <td><?= $mhs['jurusan'] ?></td>
+            </tr>
+            <?php $i++ ?>
+        <?php endforeach; ?>
+    </table>
+</body>
+
+</html>
